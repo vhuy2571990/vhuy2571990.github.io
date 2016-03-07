8a67a77fec6aa5338141363af95f4f60f682ef19
Huy Nguyen <vhuy2571990@gmail.com>
2016-03-07 22:53:18 +0700

    Fix file data.json

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 	});
 	myMod.config(function($routeProvider) {
 	  $routeProvider.when('/', {
-	    template: '<home user="$resolve.user"></home>',
+	    template: '<home user="$resolve.user.nam"></home>',
 	    resolve: {
 	      user: function($http) { return $http.get('/data.json'); }
 	    }
