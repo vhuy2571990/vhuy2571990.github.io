b53f32ced4eea92b2bb5099abdd5ad542cc0d0f5
Huy Nguyen <vhuy2571990@gmail.com>
2016-03-10 00:07:50 +0700

    Fix

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
-(function(){
-	var myMod = angular.module('myMod', ['ngRoute']);
-	myMod.component('home', {
-	  template: '<h1>Home</h1><p>Hello, {{ $ctrl.user.name }} !</p>',
-	  bindings: {
-	    user: '<'
-	  }
-	});
-	myMod.config(function($routeProvider) {
-	  $routeProvider.when('/', {
-	    template: '<home user="$resolve.user"></home>',
-	    resolve: {
-	      user: function($http) { return $http.get('/data.json'); }
-	    }
-	  });
-	});
-})();
+'use strict';
+angular.module('app', ['ngRoute'])
+		.controller('TemplateCtrl', ['$scope', '$http', function($scope, $http){
+			 $http.get('vhuy2571990.github.io/data-template.json').success(function(data) {
+		      $scope.templates = data;
+		    });
+		}])
+		.controller('TemplateDetailCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
+			  $http.get('data-template/'+ $routeParams.templateId + '.json').success(function(data){
+			  	$scope.template = data;
+			  });
+		}])
+		.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+			$locationProvider.html5Mode(true);
+
+			$routeProvider.when('/templates', {
+				templateUrl: 'vhuy2571990.github.io/partials/list-template.html',
+				controller: 'TemplateCtrl'
+			}).
+			when('/templates/:templateId', {
+				templateUrl: 'vhuy2571990.github.io/partials/template-detail.html',
+				controller: 'TemplateDetailCtrl'
+			}).
+			otherwise({
+				redirectTo: '/templates'
+			});	 
+		}]);
+	
