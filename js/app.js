5e70f6dc9938afd4f11b5e5bfe3b419e22e3e4a9
Huy Nguyen <vhuy2571990@gmail.com>
2016-03-10 19:41:43 +0700

    fix

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
 'use strict';
-angular.module('app', ['ngRoute'])
-		// .controller('TemplateCtrl', ['$scope', '$http', function($scope, $http){
-		// 	 $http.get('data-template.json').success(function(data) {
-		//       $scope.templates = data;
-		//     });
-		// }])
-		// .controller('TemplateDetailCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
-		// 	  $http.get('data-template/'+ $routeParams.templateId + '.json').success(function(data){
-		// 	  	$scope.template = data;
-		// 	  });
-		// }])
+angular.module('app', ['ui.router'])
 		.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider, $stateProvider){
 			$locationProvider.html5Mode(true);
 
