2277990a067d53e2156ee42b2f3079cb72e8a5bf
Huy Nguyen <vhuy2571990@gmail.com>
2016-03-12 11:42:14 +0700

    fix

--- js/app.js
+++ js/app.js
@@ -4,24 +4,24 @@ angular.module('app', ['ui.router'])
 			$locationProvider.html5Mode(true);
 
 			$stateProvider.state('templates', {
-				url:'/templates',
-				templateUrl: 'partials/list-template.html',
-				controller: function($scope, $http){
+				url : '/templates',
+				templateUrl : 'partials/list-template.html',
+				controller : function($scope, $http){
 					$http.get('data-template.json').then(function(response){
 						$scope.templates = response.data;
 					});
 				}
 			})
 			.state('templatedetail', function(){
-				url:'/templates/:tplId',
-				templateUrl: 'partials/tempalate-detail.html',
+				url : '/templates/:tplId',
+				templateUrl : 'partials/tempalate-detail.html',
 				controller: function($scope, $http, $stateParams){
 					$http.get('data-template/'+$stateParams.tplId+'.json').then(function(response){
 						$scope.templatesdetail = response.data.details;
 					});
 				}
 			});
-			
+
 			$urlRouterProvider.otherwise('/templates');	 
 		});
 	
