1a141791f14aba64c81b291f29455fbd0752de0e
Huy Nguyen <vhuy2571990@gmail.com>
2016-03-26 10:44:32 +0700

    fix loading icon

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 	'use strict';
 	$(window).load(function(){
 		$('body').attr('data-ajax-transitions', false);
-		$('#ajax-loading-screen').fadeOut();
+		$('#ajax-loading-screen').hide();
 	});
 })();
