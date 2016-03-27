21cd38e7fb8809f5dd201b2731a372a2f35e0901
Huy Nguyen <vhuy2571990@gmail.com>
2016-03-27 11:40:10 +0700

    fix progress bar

--- script.js
+++ script.js
@@ -3,5 +3,21 @@
 	$(window).load(function(){
 		$('body').attr('data-ajax-transitions', false);
 		$('#ajax-loading-screen').hide();
+		//progress bar
+		setTimeout(function(){
+			$('.progress').each(function(){
+				var _this = $(this);
+				var per = _this.attr('value');
+				var cur_per = 0;
+				var progress = setInterval(function(){
+					if (cur_per >= per){
+						clearInterval(progress);
+					}else{
+						cur_per += 1;
+						_this.attr('value', cur_per);
+					}
+				}, 50)
+			});
+		}, 300);
 	});
 })();
