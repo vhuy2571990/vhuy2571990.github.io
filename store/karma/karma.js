8e4dfb3f760d056de54feb688cf4a5b94d88d971
Huy Nguyen <huynguyen@bluecomgroup.com>
2017-11-14 17:25:47 +0700

    style karma templates

--- store/karma/karma.js
+++ store/karma/karma.js
@@ -1,7 +1,24 @@
 var app = new Vue({
   el: 'slider-wrapper',
   data: {
-    images: ['./images/54.png', './images/154.png', './images/824.png'],
+    images: [
+      {
+        imgSrc: './images/54.png',
+        description: `<h3>Summer Boy</h3>
+        <p>Hot item collection, let make a choice now.</p>
+        `,
+        classDes: 'is-left'},
+      {
+        imgSrc: './images/154.png',
+        description: '<h3>Donnie Karma build on this November</h3> <p>Free shipping from 3 orders item more. </p><a href="#" class="btn outline-border">View detail</a>',
+        classDes: 'is-center'},
+      {
+        imgSrc: './images/824.png',
+        description: `<h3>Top Fashion hot Melanie item</h3>
+        <p>Sale off 5% from 20 Nov to 29 Nov. </p>
+        <a href="#" class="btn outline-border">View detail</a>`,
+        classDes: 'is-right'}
+    ],
     currentNumber: 0,
     timer: null
   },
@@ -12,7 +29,7 @@ var app = new Vue({
 
   methods: {
     startRotation: function() {
-      this.timer = setInterval(this.next, 2000);
+      this.timer = setInterval(this.next, 6000);
     },
 
     stopRotation: function() {
