85a4bb26e3943e50edd59b48dbe3c962b18e0e5c
Huy Nguyen <huynguyen@bluecomgroup.com>
2017-11-15 17:54:43 +0700

    karma

--- store/karma/js/karma.js
+++ store/karma/js/karma.js
@@ -3,17 +3,17 @@ var app = new Vue({
   data: {
     images: [
       {
-        imgSrc: './images/54.png',
+        imgSrc: './images/tinified/54.png',
         description: `<h3>Summer Set Collection</h3>
         <p>Hot item collection, let make a choice now.</p>
         `,
         classDes: 'is-left'},
       {
-        imgSrc: './images/154.png',
+        imgSrc: './images/tinified/154.png',
         description: '<h3>Donnie Karma build on this November</h3> <p>Free shipping from 3 orders item more. </p><a href="#" class="btn outline-border">View detail</a>',
         classDes: 'is-center'},
       {
-        imgSrc: './images/824.png',
+        imgSrc: './images/tinified/824.png',
         description: `<h3>Top Fashion hot Melanie item</h3>
         <p>Sale off 5% from 20 Nov to 29 Nov. </p>
         <a href="#" class="btn outline-border">View detail</a>`,
