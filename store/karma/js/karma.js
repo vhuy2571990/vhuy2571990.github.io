269af89b56aeaee31ffb9541013168b458a57864
Huy Nguyen <huynguyen@bluecomgroup.com>
2017-11-16 13:05:37 +0700

    FIX KARMA

--- store/karma/js/karma.js
+++ store/karma/js/karma.js
@@ -1,34 +1,43 @@
-const Foo = { template: '<div>foo</div>' }
+const Foo = {
+  template: '<div>foo</div>'
+}
 
-const routes = [
-    {path: '/foo', component: Foo}
-]
+const routes = [{
+  path: '/foo',
+  component: Foo,
+}]
 
-const router = new VueRouter({routes});
-var app = new Vue({
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+const appVue = new Vue({
   router,
-  el: 'slider-wrapper',
+  el: '#app-wrapper',
   data: {
-    images: [
-      {
-        imgSrc: './images/tinified/54.png',
-        description: `<h3>Summer Set Collection</h3>
-        <p>Hot item collection, let make a choice now.</p>
-        `,
-        classDes: 'is-left'},
-      {
-        imgSrc: './images/tinified/154.png',
-        description: '<h3>Donnie Karma build on this November</h3> <p>Free shipping from 3 orders item more. </p><a href="#" class="btn outline-border">View detail</a>',
-        classDes: 'is-center'},
-      {
-        imgSrc: './images/tinified/824.png',
-        description: `<h3>Top Fashion hot Melanie item</h3>
-        <p>Sale off 5% from 20 Nov to 29 Nov. </p>
-        <a href="#" class="btn outline-border">View detail</a>`,
-        classDes: 'is-right'}
+    images: [{
+          imgSrc: './images/tinified/54.png',
+          description: `<h3>Summer Set Collection</h3>
+          <p>Hot item collection, let make a choice now.</p>
+          `,
+          classDes: 'is-left'
+        },
+        {
+          imgSrc: './images/tinified/154.png',
+          description: '<h3>Donnie Karma build on this November</h3> <p>Free shipping from 3 orders item more. </p><a href="#" class="btn outline-border">View detail</a>',
+          classDes: 'is-center'
+        },
+        {
+          imgSrc: './images/tinified/824.png',
+          description: `<h3>Top Fashion hot Melanie item</h3>
+          <p>Sale off 5% from 20 Nov to 29 Nov. </p>
+          <a href="#" class="btn outline-border">View detail</a>`,
+          classDes: 'is-right'
+        }
     ],
     currentNumber: 0,
-    timer: null
+    timer: null,
   },
 
   mounted: function() {
@@ -46,7 +55,7 @@ var app = new Vue({
     },
 
     next: function() {
-      (this.currentNumber === this.images.length - 1) ? this.currentNumber = 0 : this.currentNumber += 1 ;
+      (this.currentNumber === this.images.length - 1) ? this.currentNumber = 0: this.currentNumber += 1;
     },
 
     prev: function() {
@@ -54,7 +63,7 @@ var app = new Vue({
     },
 
     isActive: function(index) {
-      return (index === this.currentNumber) ? true: false;
+      return (index === this.currentNumber) ? true : false;
     }
   }
-}).$mount('#app-wrapper');
+});
