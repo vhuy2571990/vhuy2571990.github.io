6b6a47c062b4c205f8a713211a24c93e7583ffea
vhuy2571990 <vhuy2571990>
2018-03-25 13:15:55 +0700

    push master

--- store/karma/js/data.js
+++ store/karma/js/data.js
@@ -24,10 +24,10 @@ const listSlimShirt = [
    {name: 'Casual Slim Fit White Egyptian Cotton Soft Collar Shirt', image: './images/men-shirt/58417.jpg', price: 39.95},
    {name: 'Casual Slim Fit Navy Red Gingham Egyptian Cotton Collar Shirt', image: './images/men-shirt/58423.jpg', price: 39.95},
    {name: 'Casual Slim Indigo Brushed Egyptian Cotton Collar Shirt', image: './images/men-shirt/58572.jpg', listImages: ['./images/men-shirt/56868_alt1.jpg', './images/men-shirt/56868_alt2.jpg'], price: 39.95},
-   {name: 'Non-Iron Casual Slim Blue Stripe Cotton Slim Fit Shirt', image: './images/men-shirt/58573.jpg', price: 39.95},
-   {name: 'Casual Slim Red Navy Check Egyptian Cotton  Collar Shirt', image: './images/men-shirt/58580.jpg', price: 39.95},
-   {name: 'Casual Slim Country Check Brushed Egyptian Cotton  Shirt', image: './images/men-shirt/images.jpg', price: 39.95},
-   {name: 'Slim Fit Navy Twill Button Cuff Shirt', image: './images/men-shirt/57128.jpg', listImages: ['./images/men-shirt/57128_alt1.jpg', './images/men-shirt/57128_alt2.jpg'], price: 39.95}
+   // {name: 'Non-Iron Casual Slim Blue Stripe Cotton Slim Fit Shirt', image: './images/men-shirt/58573.jpg', price: 39.95},
+   // {name: 'Casual Slim Red Navy Check Egyptian Cotton  Collar Shirt', image: './images/men-shirt/58580.jpg', price: 39.95},
+   // {name: 'Casual Slim Country Check Brushed Egyptian Cotton  Shirt', image: './images/men-shirt/images.jpg', price: 39.95},
+   // {name: 'Slim Fit Navy Twill Button Cuff Shirt', image: './images/men-shirt/57128.jpg', listImages: ['./images/men-shirt/57128_alt1.jpg', './images/men-shirt/57128_alt2.jpg'], price: 39.95}
 ]
 
 const dataMenu = {
